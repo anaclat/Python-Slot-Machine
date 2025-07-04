5b4abc826e50b83e095a5985ea375b58e33e1663
Ana Ronzani <aninhateixeira1810@gmail.com>
2025-07-04 14:01:40 +0000

    add lines

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+MAX_LINES = 3
+
 def deposit():
     while True:
         amount = input('What would you like to deposit? $')
@@ -11,4 +13,24 @@ def deposit():
             print(f'Please enter a number.')
     return amount
 
-deposit()
+
+def numbers_of_line():
+    while True:
+        lines = input('Enter the number of lines to bet on (1-'  + str(MAX_LINES)  + '): ')
+        if lines.isdigit():
+            lines = int(lines)
+            if 1 <= lines <= MAX_LINES:
+                break
+            else:
+                print(f'Enter a valid numbers of lines.')
+        else:
+            print(f'Please enter a number.')
+    return lines
+
+
+def main():
+    balance =  deposit()
+    lines = numbers_of_line()
+    print(balance,lines)
+
+main()
